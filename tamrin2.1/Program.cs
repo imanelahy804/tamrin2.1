48e8b5c2c5a13dad0e293efad90b73261b3f021a
radin3607 <radin3607@yahoo.com>
2022-03-04 05:40:54 -0800

    Add project files.

---
+++ tamrin2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("please enter interger array:");
+var input = Console.ReadLine();
+string[] inputArray = input.Split(',');
+for (int i = 0; i < inputArray.Length; i++)
+{
+    inputArray[i] = inputArray[i].Trim();
+}
+int[] inputNumber = new int[inputArray.Length];
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    inputNumber[i] = Convert.ToInt32(inputArray[i]);
+}
+Console.WriteLine("1-min");
+int t = Convert.ToInt32(Console.ReadLine());
+switch (t)
+{
+    case 1:
+        Console.WriteLine(min(inputNumber);
+        break;
+    default:
+        break;
+}
+
+
+int min(int[] array,)
+{
+    int min = array[0];
+    foreach (var item in array)
+    {
+        if (item<min)
+        {
+            min = item;
+        }
+    }
+    return min;
+}
